1c5d64a401ac0e3d5c33c23b03d9536d55497fae
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-11 15:09:50 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import About from './pages/About'
+import Board from './pages/Board'
+import Home from './pages/Home'
+import Notfound from './pages/Notfound'
+import BoardDetail from './pages/BoardDetail'
+function App() {
+
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/board' element={<Board />} />
+        <Route path='/board/:id' element={<BoardDetail />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
